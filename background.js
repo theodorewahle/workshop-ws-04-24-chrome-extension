063f7ea9c39a46326521af36a668baf583333839
Theodore Wahle <theodorewahle@gmail.com>
2019-04-24 21:23:54 -0400

    Completed workshop

---
+++ background.js
@@ -0,0 +1,21 @@
+//Setting badge/button of the extension.
+chrome.browserAction.setBadgeText({ text: 'OFF' });
+
+// onclick method
+chrome.browserAction.onClicked.addListener((tab) => {
+    enable = !enable;
+    // if enabled run content.js else, reload page
+    if (enable) {
+        chrome.browserAction.setBadgeText({text: 'ON'});
+        chrome.tabs.executeScript(null, { file: 'content.js'});
+    } else {
+        chrome.browserAction.setBadgeText({ text: 'OFF'});
+        chrome.tabs.executeScript(tab.id, {code: 'window.location.reload();'});
+    }
+
+    // store state of button
+    // with callback for debugging
+    // chrome.storage.sync.set({"enable": enable}, () => console.log('Enable set to: ', enable));
+    // w/o callback
+    chrome.storage.sync.set({"enable": enable});
+})
